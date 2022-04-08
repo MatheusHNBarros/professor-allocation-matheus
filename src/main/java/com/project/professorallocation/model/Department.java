e826918009165617297f92fdc40d5a104bb931e3
Barros <matheushnbarross@gmail.com>
2022-04-07 21:14:47 -0300

    Add Department entity

---
+++ src/main/java/com/project/professorallocation/model/Department.java
@@ -0,0 +1,46 @@
+package com.project.professorallocation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departament")
+
+public class Department {
+
+	@Column(length = 100, nullable = false)
+	private String nome;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	public Department() {
+		super();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [nome=" + nome + ", id=" + id + "]";
+	}
+
+}
